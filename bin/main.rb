723915e6bbee67210a0938b9884951917e1ce880
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-31 22:28:15 +0600

    Added a user input method to get data

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,12 @@
+require "highline/import"
+
 require_relative '../lib/payment.rb'
 
+def get_user_input
+  amount = ask "Loan Amount: "
+  terms = ask "Term (in months): "
+  interest = ask "Annual Interest Rate (%): "
+  request_date = ask "Disbursment Date(MM/DD/YYYY): "
+end
+
+get_user_input()
