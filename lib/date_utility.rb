2e8cb3c60ea62db9b88b322af8fe9ed00da773f2
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-31 22:40:03 +0600

    Added class method to output the preffered format.

--- lib/date_utility.rb
+++ lib/date_utility.rb
@@ -5,4 +5,9 @@ class DateConverter
     splited_date = date.split('/')
     Date.parse(splited_date[1] + '/' + splited_date[0]+ '/'+ splited_date[2])
   end
+
+  def self.preffered_date_format(date)
+    splited_date = date.strftime("%F").split('-')
+    splited_date[1] + '/' + splited_date[2]+ '/'+ splited_date[0]
+  end
 end
